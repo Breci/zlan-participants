3b6901ff9601aadf9b873d529efa24df357e81da
Breci <culas.brice@gmail.com>
2020-08-19 09:42:38 +0200

    fixed lang

--- pages/index.js
+++ pages/index.js
@@ -14,67 +14,65 @@ export default function Home({ streams, users }) {
     });
   }, [streams, users]);
   return (
-    <Html lang="fr">
-      <div className={styles.container}>
-        <Head>
-          <title>ZLAN</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+    <div className={styles.container}>
+      <Head>
+        <title>ZLAN</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <main className={styles.main}>
-          <h1 className={styles.title}>ZLAN</h1>
+      <main className={styles.main}>
+        <h1 className={styles.title}>ZLAN</h1>
 
-          <p className={styles.description}>Découvrez les participants</p>
+        <p className={styles.description}>Découvrez les participants</p>
 
-          <div className={styles.list}>
-            {usersInfo.map((userInfo) => (
-              <a
-                target="_blank"
-                rel="noreferrer noopener"
-                href={`https://www.twitch.tv/${userInfo.user.login}`}
+        <div className={styles.list}>
+          {usersInfo.map((userInfo) => (
+            <a
+              target="_blank"
+              rel="noreferrer noopener"
+              href={`https://www.twitch.tv/${userInfo.user.login}`}
+            >
+              <div
+                className={styles["anim-container"]}
+                style="position: relative"
               >
-                <div
-                  className={styles["anim-container"]}
-                  style="position: relative"
-                >
-                  <div className={styles["top-left-corner"]}></div>
-                  <div className={styles["left-side"]}></div>
-                  <div className={styles["bottom-side"]}></div>
-                  <div className={styles["bottom-right-corner"]}></div>
-                  <div className={styles["img-container"]}>
-                    <img
-                      src={userInfo.stream.thumbnail_url
-                        .replace("{width}", "800")
-                        .replace("{height}", "450")}
-                      className={styles["thumbnail"]}
-                      alt={userInfo.stream.user_name}
-                    />
-                    <div className={styles["viewers"]}>
-                      {userInfo.stream.viewer_count} viewers
-                    </div>
-                    <div className={styles["streamer-name"]}>
-                      {userInfo.stream.user_name}
-                    </div>
+                <div className={styles["top-left-corner"]}></div>
+                <div className={styles["left-side"]}></div>
+                <div className={styles["bottom-side"]}></div>
+                <div className={styles["bottom-right-corner"]}></div>
+                <div className={styles["img-container"]}>
+                  <img
+                    src={userInfo.stream.thumbnail_url
+                      .replace("{width}", "800")
+                      .replace("{height}", "450")}
+                    className={styles["thumbnail"]}
+                    alt={userInfo.stream.user_name}
+                  />
+                  <div className={styles["viewers"]}>
+                    {userInfo.stream.viewer_count} viewers
+                  </div>
+                  <div className={styles["streamer-name"]}>
+                    {userInfo.stream.user_name}
                   </div>
                 </div>
-              </a>
-            ))}
-          </div>
-        </main>
+              </div>
+            </a>
+          ))}
+        </div>
+      </main>
 
-        <footer className={styles.footer}>
-          Créé par{" "}
-          <a
-            rel="noreferrer noopener"
-            href="https://www.twitter.com/Brecii"
-            target="_blank"
-            className={styles.logo}
-          >
-            Breci
-          </a>
-        </footer>
-      </div>
-    </Html>
+      <footer className={styles.footer}>
+        Créé par{" "}
+        <a
+          rel="noreferrer noopener"
+          href="https://www.twitter.com/Brecii"
+          target="_blank"
+          className={styles.logo}
+        >
+          Breci
+        </a>
+      </footer>
+    </div>
   );
 }
 
