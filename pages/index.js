d149899e8f10cc183bde694fa68815c9a5b6ed1e
Breci <culas.brice@gmail.com>
2020-08-19 09:06:28 +0200

    added links to streams

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
-import {getLiveStreams} from "../twitch";
-import {getStreamers} from "../utils";
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
+import { getLiveStreams, getUsersInfo } from "../twitch";
+import { getStreamers } from "../utils";
+import { useMemo } from "preact/hooks";
 
-export default function Home({streams}) {
+export default function Home({ streams, users }) {
+  const usersInfo = useMemo(() => {
+    return streams.map((stream) => {
+      return {
+        stream: stream,
+        user: users.find((u) => u.id === stream.user_id),
+      };
+    });
+  }, [streams, users]);
   return (
     <div className={styles.container}>
       <Head>
@@ -12,46 +21,66 @@ export default function Home({streams}) {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          ZLAN
-        </h1>
+        <h1 className={styles.title}>ZLAN</h1>
 
-        <p className={styles.description}>
-          Découvrez les participants
-        </p>
+        <p className={styles.description}>Découvrez les participants</p>
 
-         <div className={styles.list}>
-             {streams.map(stream=>(      <div className={styles["anim-container"]} style="position: relative">
-                 <div className={styles["top-left-corner"]}></div>
-                 <div className={styles["left-side"]}></div>
-                 <div className={styles["bottom-side"]}></div>
-                 <div className={styles["bottom-right-corner"]}></div>
-                 <div className={styles["img-container"]}>
-                     <img
-                     src={stream.thumbnail_url.replace("{width}","800").replace("{height}","450")}
-                      className={styles["thumbnail"]}
-                     alt={stream.user_name}
-                 />
-                 <div className={styles["viewers"]}>{ stream.viewer_count } viewers</div>
-                 <div className={styles["streamer-name"]}>{ stream.user_name }</div>
-             </div>
-                 </div>
-                 ))}
-         </div>
+        <div className={styles.list}>
+          {usersInfo.map((userInfo) => (
+            <a
+              target="_blank"
+              href={`https://www.twitch.tv/${userInfo.user.login}`}
+            >
+              <div
+                className={styles["anim-container"]}
+                style="position: relative"
+              >
+                <div className={styles["top-left-corner"]}></div>
+                <div className={styles["left-side"]}></div>
+                <div className={styles["bottom-side"]}></div>
+                <div className={styles["bottom-right-corner"]}></div>
+                <div className={styles["img-container"]}>
+                  <img
+                    src={userInfo.stream.thumbnail_url
+                      .replace("{width}", "800")
+                      .replace("{height}", "450")}
+                    className={styles["thumbnail"]}
+                    alt={userInfo.stream.user_name}
+                  />
+                  <div className={styles["viewers"]}>
+                    {userInfo.stream.viewer_count} viewers
+                  </div>
+                  <div className={styles["streamer-name"]}>
+                    {userInfo.stream.user_name}
+                  </div>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
       </main>
 
       <footer className={styles.footer}>
-          Créé par{' '}
-          <a href="https://www.twitter.com/Brecii" target="_blank" className={styles.logo} >Breci</a>
+        Créé par{" "}
+        <a
+          href="https://www.twitter.com/Brecii"
+          target="_blank"
+          className={styles.logo}
+        >
+          Breci
+        </a>
       </footer>
     </div>
-  )
+  );
 }
 
 export async function getServerSideProps(context) {
+  const streams = await getLiveStreams(getStreamers());
+  const usersIds = streams.map((stream) => stream.user_id);
   return {
     props: {
-      streams: await getLiveStreams(getStreamers())
+      streams: await getLiveStreams(getStreamers()),
+      users: await getUsersInfo(usersIds),
     }, // will be passed to the page component as props
-  }
+  };
 }
--- twitch/index.js
+++ twitch/index.js
@@ -1,34 +1,68 @@
-import axios from 'axios'
+import axios from "axios";
 
-function chunkArray(myArray, chunk_size){
-    const arrayLength = myArray.length;
-    const tempArray = [];
+function chunkArray(myArray, chunk_size) {
+  const arrayLength = myArray.length;
+  const tempArray = [];
 
-    for (let index = 0; index < arrayLength; index += chunk_size) {
-        const myChunk = myArray.slice(index, index+chunk_size);
-        // Do something if you want with the group
-        tempArray.push(myChunk);
-    }
+  for (let index = 0; index < arrayLength; index += chunk_size) {
+    const myChunk = myArray.slice(index, index + chunk_size);
+    // Do something if you want with the group
+    tempArray.push(myChunk);
+  }
 
-    return tempArray;
+  return tempArray;
 }
 
-async function getAuthToken(){
-    return axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_SECRET}&grant_type=client_credentials`)
-        .then(resp=>resp.data.access_token)
+async function getAuthToken() {
+  // TODO optimize that
+  return axios
+    .post(
+      `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_SECRET}&grant_type=client_credentials`
+    )
+    .then((resp) => resp.data.access_token);
 }
 
-export async function getLiveStreams(streamsId){
-    const token = await getAuthToken()
-    
-    const streams =  (await Promise.all(chunkArray(streamsId, 100).map(ids => axios.get("https://api.twitch.tv/helix/streams", {
-        params: {
-            user_login: ids
-        },
-        headers:{
-            'Authorization': `Bearer ${token}`,
-            'Client-ID': process.env.TWITCH_CLIENT_ID
-        }
-    }).then(resp=>resp.data.data)))).flat()
-    return streams
+export async function getLiveStreams(streamsId) {
+  const token = await getAuthToken();
+
+  const streams = (
+    await Promise.all(
+      chunkArray(streamsId, 100).map((ids) =>
+        axios
+          .get("https://api.twitch.tv/helix/streams", {
+            params: {
+              user_login: ids,
+            },
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Client-ID": process.env.TWITCH_CLIENT_ID,
+            },
+          })
+          .then((resp) => resp.data.data)
+      )
+    )
+  ).flat();
+  return streams;
+}
+export async function getUsersInfo(streamsId) {
+  const token = await getAuthToken();
+
+  const users = (
+    await Promise.all(
+      chunkArray(streamsId, 100).map((ids) =>
+        axios
+          .get("https://api.twitch.tv/helix/users", {
+            params: {
+              id: ids,
+            },
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Client-ID": process.env.TWITCH_CLIENT_ID,
+            },
+          })
+          .then((resp) => resp.data.data)
+      )
+    )
+  ).flat();
+  return users;
 }
